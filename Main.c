ebae48775aef58499eac71bf1d1308f6985f8c0f
Tharindu-Gimhana <167947975+Tharindu-Gimhana@users.noreply.github.com>
2025-05-26 20:56:30 +0530

    Added decryption function to Main.c

--- Main.c
+++ Main.c
@@ -3,7 +3,10 @@
 #include <string.h>
 #include"queue.h"
 
-void encrypt(circularQueue *q,char *text,int key){
+
+//function for the encryption
+
+void encrypt(circularQueue *q,char *text,int key){                    
     for(int i=0;text[i]!='\0';i++){
         if(isalpha(text[i])){
             int isLower = islower(text[i]);  //To check the lowercase 
@@ -19,6 +22,27 @@ void encrypt(circularQueue *q,char *text,int key){
         }
     }
 }
+
+
+//function for the decryption
+
+void decrypt(circularQueue *q,char *text,int key){      
+    for(int i=0;text[i]!='\0';i++){
+        if(isalpha(text[i])){
+            int isLower = islower(text[i]);  
+
+            text[i] = toupper(text[i]);
+            int index = getindex(q, text[i]);
+            int shiftindex = (index - key + queue_size) % queue_size;  //can handle negative values
+            text[i] = getinchar(q, shiftindex);
+
+            if (isLower) {
+                text[i] = tolower(text[i]);  
+            }
+        }
+    }
+}
+
 
 int main(){
     return 0;
